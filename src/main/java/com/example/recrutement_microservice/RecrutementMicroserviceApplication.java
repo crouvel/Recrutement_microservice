9c9265535d041a29e1ac3133dc356269553094d9
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-28 02:06:42 +0100

    Initial commit

---
+++ src/main/java/com/example/recrutement_microservice/RecrutementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.recrutement_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutementMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecrutementMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recrutement_microservice/RecrutementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recrutement_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutementMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
